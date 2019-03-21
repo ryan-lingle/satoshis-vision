0a3c4ec268cf08b29a38518e4f571f27801ecbaf
Ryan Lingle <lingleryan@gmail.com>
2019-03-21 16:26:24 -0400

    make arguments a string

--- app/controllers/api/v1/lnd_controller.rb
+++ app/controllers/api/v1/lnd_controller.rb
@@ -5,8 +5,8 @@ class Api::V1::LndController < ApplicationController
     edits = JSON.parse(params[:edits])
     invoice = LnService.add_invoice(
       memo: "Publish Edit @ toshi.vision",
-      value: edits.size * 100,
-      expiry: 300
+      value: "#{edits.size * 100}",
+      expiry: '300'
     )
     InvoicesWorker.perform_async(
       payment_request: invoice.payment_request,
