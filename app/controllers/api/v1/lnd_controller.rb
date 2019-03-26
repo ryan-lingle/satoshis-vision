b8f1c1cba10eb1df9913b76f56d928c100f42cb7
Ryan Lingle <lingleryan@gmail.com>
2019-03-26 09:16:50 -0400

    update concurrency

--- app/controllers/api/v1/lnd_controller.rb
+++ app/controllers/api/v1/lnd_controller.rb
@@ -6,8 +6,12 @@ class Api::V1::LndController < ApplicationController
 
     value = edits.size * 100
 
-    invoice_request = Lnrpc::Invoice.new({ memo: "Editing the Vision", value: value, expiry: 300 })
-    invoice = LnService.grpc_client.add_invoice(invoice_request)
+
+    invoice = LnService.add_invoice({
+      memo: "Editing the Vision",
+      value: value,
+      expiry: 300
+    })
 
     InvoicesWorker.perform_async(
       payment_request: invoice.payment_request,
